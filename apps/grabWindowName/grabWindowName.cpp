1e9c35a16cf610693d487ca19ca7bc3c5a813aee
bezbahen0 <bezbahen0@mail.ru>
2020-06-29 18:17:40 +0300

    add application for get windows names

---
+++ apps/grabWindowName/grabWindowName.cpp
@@ -0,0 +1,59 @@
+#include <X11/Xlib.h>
+#include <X11/X.h>
+#include <X11/Xutil.h>
+
+#include <iostream>
+
+Window* getWindowList(Display* disp, unsigned long* len)
+{
+    Atom prop = XInternAtom(disp, "_NET_CLIENT_LIST", false);
+    Atom type;
+    int form;
+    unsigned long remain;
+    unsigned char* list;
+
+    if (XGetWindowProperty(disp,XDefaultRootWindow(disp),prop,0,1024, false,33,
+                &type,&form,len,&remain,&list) != Success) 
+    {  
+        return nullptr;
+    }
+
+    return (Window*) list;
+}
+char* getWindowName(Display* disp, Window win)
+{
+    Atom prop = XInternAtom(disp, "WM_NAME", false);
+    Atom type;
+    int form;
+    unsigned long remain;
+    unsigned long len;
+    unsigned char* list;
+
+
+    if (XGetWindowProperty(disp, win, prop, 0, 1024, false, AnyPropertyType,
+                &type, &form, &len, &remain, &list) != Success) 
+    {
+        return nullptr;
+    }
+
+    return (char*) list;
+}
+
+int main()
+{
+    unsigned long len;
+    XKeyEvent esend;
+    Display* disp = XOpenDisplay(nullptr);
+    Window* list;
+    char* name;
+
+    list = (Window*) getWindowList(disp, &len);
+    for (int i = 0; i < (int)len; i++) 
+    {
+        name = getWindowName(disp, list[i]);
+        std::cout << i << ": " << name << std::endl;
+        delete(name);
+    }
+
+    return 0;
+}
