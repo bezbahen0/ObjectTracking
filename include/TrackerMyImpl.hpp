4da43c6aaf67b3ed5331b9faa9ec6943b5127dbd
bezbahen0 <bezbahen0@mail.ru>
2020-07-03 18:28:51 +0300

    trash commit
    trash
    
    Delete tz.txt

--- include/TrackerMyImpl.hpp
+++ include/TrackerMyImpl.hpp
@@ -7,6 +7,7 @@ class TrackerMyImpl : public cv::Tracker
 {
 public:
     virtual ~TrackeriMyImpl() {}
+    TrackerMyImpl() {isInit = 0;}
 
     virtual void read(const FileNode&) CV_OVERRIDE {}
     virtual void write(FileStorage&) const CV_OVERRIDE {}
@@ -18,7 +19,6 @@ protected:
     virtual bool updateImpl(const cv::Mat& image, cv::Rect2d& boundingBox) CV_OVERRIDE;
 
 private:
-    TrackerMyImpl() {isInit = 0;}
 };
 
 #endif /* TRACKERMYIMPL_HPP */
